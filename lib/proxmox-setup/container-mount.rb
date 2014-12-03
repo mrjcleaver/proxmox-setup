917ccc5476893e039a7567bebeea8d950a194beb
Martin Cleaver <Martin.Cleaver@BlendedPerspectives.com>
2014-12-02 22:27:37 -0500

    Added container mount scripts, so you can force your ssh key onto every container

--- lib/proxmox-setup/container-mount.rb
+++ lib/proxmox-setup/container-mount.rb
@@ -1,5 +1,16 @@
 def container_mount(vm)
+  nodes_dir ='/etc/pve/nodes/'
+  node_names=proxmox_ssh(vm, "ls #{nodes_dir}").chomp()
+  # TODO: make this work with multiple names, or even detect them
+  open_vz_dir=nodes_dir+node_names+'/openvz/'
+  puts open_vz_dir
 
-proxmox_scp(vm, 'postinstall/mount.vps', '/etc/pve/nodes/proxmox/openvz/mount.vps')
-proxmox_scp(vm, 'postinstall/talk-to-ct.sh', '/etc/pve/nodes/proxmox/openvz/talk-to-ct.sh')
-proxmox_scp(vm, 'postinstall/run-in-ct.sh', '/etc/pve/nodes/proxmox/openvz/run-in-ct.sh')
+  ## https://wiki.openvz.org/Man/vzctl.8#ACTION_SCRIPTS
+  proxmox_scp(vm, 'postinstall/vps.mount', open_vz_dir+'vps.mount')
+  proxmox_scp(vm, 'postinstall/talk-to-ct.sh', open_vz_dir+'talk-to-ct.sh')
+  proxmox_scp(vm, 'postinstall/run-in-ct.sh', open_vz_dir+'run-in-ct.sh')
+
+  id_file=ENV['HOME']+"/.ssh/id_rsa.pub"
+  proxmox_scp(vm, id_file,open_vz_dir+'id_rsa.pub')
+
+end
--- postinstall/talk-to-ct.sh
+++ postinstall/talk-to-ct.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sleep 10
+sleep 10 # hoping it will boot by then
 
-/usr/sbin/vzctl runscript ${VEID} /etc/pve/nodes/proxmox/openvz/run-in-ct
+id_file=/etc/pve/nodes/proxmox-mm/openvz/id_rsa.pub
+cat $id_file | vzctl exec ${VEID} "mkdir /root/.ssh; cat - > /root/.ssh/authorized_keys"
+
+
+/usr/sbin/vzctl runscript ${VEID} /etc/pve/nodes/proxmox-mm/openvz/run-in-ct.sh
